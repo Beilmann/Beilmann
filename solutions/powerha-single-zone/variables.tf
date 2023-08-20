3858c96bf656e0d2381e9c419b9fd842b64883da
Arnold Beilmann <arnoldbeilmann@MacBook-Pro-von-Arnold.local>
2023-08-20 17:49:52 +0200

    aix demo

---
+++ solutions/powerha-single-zone/variables.tf
@@ -0,0 +1,9 @@
+variable "powervs_zone" {
+  description = "IBM Cloud data center location where IBM PowerVS infrastructure will be created."
+  type        = string
+}
+
+variable "prefix" {
+  description = "A unique identifier for resources. Must begin with a lowercase letter and end with a lowercase letter or number. This prefix will be prepended to any resources provisioned by this template. Prefixes must be 16 or fewer characters."
+  type        = string
+}
